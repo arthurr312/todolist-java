dc84375177c4f18eee2b2ba0c48f4422e1fd3d9d
Arthur Silva <arthur10.silva312@gmail.com>
2023-10-10 17:33:50 -0300

    first commit

---
+++ src/main/java/br/com/arthur/todolist/user/UserController.java
@@ -0,0 +1,25 @@
+package br.com.arthur.todolist.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private IUserRepository userRepository;
+    @PostMapping("")
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+        if(user != null) {
+            return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body("Usuário já existe");
+        }
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+    }
+}
